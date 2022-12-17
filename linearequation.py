446b10b48414056052dd90e7aeee2d285d1fcc70
Alfred Ochieng <111696034+Mesh-code1@users.noreply.github.com>
2022-12-17 18:39:47 +0300

    Add files via upload
    Code for predicting output y values for linear and polynomial equation

---
+++ linearequation.py
@@ -0,0 +1,9 @@
+def calculate_y_polynomial(x):
+  y = -0.0024 * x**2 + 0.0958 * x + 2.4939
+  print(y)
+
+def calculate_y_linear(x):
+  y = 0.02807 * x + 2.92739
+  print(y)
+
+calculate_y_linear(20.5)
